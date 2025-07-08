6a049b203ab9576b4d33dd7be3b2b4d9337d1bb2
unknown <you@example.com>
2025-07-08 13:04:35 +1000

    first commit

---
+++ portfolio/src/Components/ProjectsSection.jsx
@@ -0,0 +1,112 @@
+import { Description } from "@radix-ui/react-toast";
+import { ArrowRight, ExternalLinkIcon, GithubIcon } from "lucide-react";
+
+const projects = [
+    {
+     id: 1,
+     title: "Saas Landingpage",
+     description:"A beautiful page",   
+     image: "/projects/hello1.jpeg",
+     tags: ["React", "Tailwind", "Supabase"],
+     demoUrl: "#",
+     githubUrl: "#",
+    },
+    {
+     id: 2,
+     title: "4m",
+     description:"A  page",   
+     image: "/projects/hello2.jpeg",
+     tags: ["React", "yar", "Supabase"],
+     demoUrl: "#",
+     githubUrl: "#",
+    },
+    {
+     id: 3,
+     title: "4mv",
+     description:"A beautiful ",   
+     image: "/projects/hello3.jpg",
+     tags: ["React", "hora", "Supabase"],
+     demoUrl: "#",
+     githubUrl: "#",
+    },
+    {
+     id: 2,
+     title: "4m",
+     description:"A  page",   
+     image: "/projects/hello2.jpeg",
+     tags: ["React", "yar", "Supabase"],
+     demoUrl: "#",
+     githubUrl: "#",
+    },
+     {
+     id: 3,
+     title: "4mv",
+     description:"A beautiful ",   
+     image: "/projects/hello3.jpg",
+     tags: ["React", "hora", "Supabase"],
+     demoUrl: "https://gears.kunyo.co",
+     githubUrl: "#",
+    },
+    {
+     id: 1,
+     title: "Saas Landingpage",
+     description:"A beautiful page",   
+     image: "/projects/hello1.jpeg",
+     tags: ["React", "Tailwind", "Supabase"],
+     demoUrl: "#",
+     githubUrl: "#",
+    },
+    
+   
+
+]
+
+
+export const ProjectsSection= () =>{
+    return <section id="projects" className="py-24 px-4 relative">
+        <div className="container mx-auto max-w-5xl">
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Featured<span className="text-primary"> Projects </span> </h2>
+            <p className="text-center text-mutedforeground mb-12 max-w-2xl mx-auto">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore vel earum, ullam alias nulla fuga.
+                At optio, eaque cupiditate atque sit nesciunt laudantium vitae magni? Odio nemo et modi. Accusamus!
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {projects.map((project, key) => (
+                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                        <div className="h-48 overflow-hidden">
+                            <img src={project.image} alt={project.title} className="w-full h-full object-hover transition-transform duration-500 group-hover:scale-110"/>
+                        </div>
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">{tag}</span>
+                                ))}
+                            </div>
+                        </div>
+                        <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
+                        <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+                        <div className="flex justify-between items-center">
+                            <div className="flex space-x-3">
+                                <a href={project.demoUrl} target="_blank" 
+                                className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                    <ExternalLinkIcon size={20}/></a>
+                                <a href={project.githubUrl} target="_blank" 
+                                className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                    <GithubIcon size={20}/></a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="flex text-center mt-12">
+                <a className="cosmic-button w-fit flex items-center mx-auto gap-2" target="_blank"
+                 href="https://www.behance.net/trishalbashi">Check My Behance<ArrowRight size={16}/></a>
+                 <a className="cosmic-button w-fit flex items-center mx-auto gap-2" target="_blank"
+                 href="https://github.com/Beckhamx23">Check My Github <ArrowRight size={16}/></a>
+            </div>
+        </div>
+
+    </section>
+};
